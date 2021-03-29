3fc1c60549ff0fa84c08d9217a0987d8fef0bfea
Sherin Cheriyan <70449566+scheriyan@users.noreply.github.com>
2021-03-29 01:16:25 -0400

    Update Courses.java

--- src/main/java/com/registration/springboot/model/Courses.java
+++ src/main/java/com/registration/springboot/model/Courses.java
@@ -12,45 +12,55 @@ import javax.persistence.Table;
 public class Courses {
 
     private long id;
+    private String courseNumber;
     private String courseName;
-    private String time;
     private String semester;
- 
-    public Courses() {
-  
+    private String type;
+    private String preRequisites;
+    private String partOfDay;
+    private String time;
+    private String day;
+    private String faculty;
+
+    public Courses(){
+
     }
- 
-    public Courses(String courseName, String time, String semester) {
-         this.courseName = courseName;
-         this.time = time;
-         this.semester = semester;
+
+    public Courses(String courseNumber, String courseName, String semester, String type,
+                   String preRequisites, String partOfDay, String time, String day, String faculty) {
+        this.courseNumber = courseNumber;
+        this.courseName = courseName;
+        this.semester = semester;
+        this.type = type;
+        this.preRequisites = preRequisites;
+        this.partOfDay = partOfDay;
+        this.time = time;
+        this.day = day;
+        this.faculty = faculty;
     }
+
  
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-        public long getId() {
+    public long getId() {
         return id;
     }
     public void setId(long id) {
         this.id = id;
     }
- 
-    @Column(name = "course_name", nullable = false)
+
+    @Column(name = "courseNumber", nullable = false)
+    public String getCourseNumber() { return courseNumber; }
+    public void setCourseNumber(String courseNumber) { this.courseNumber = courseNumber; }
+
+    @Column(name = "courseName", nullable = false)
     public String getCourseName() {
         return courseName;
     }
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
- 
-    @Column(name = "time", nullable = false)
-    public String getTime() {
-        return time;
-    }
-    public void setTime(String time) {
-        this.time = time;
-    }
- 
+
     @Column(name = "semester", nullable = false)
     public String getSemester() {
         return semester;
@@ -59,10 +69,48 @@ public class Courses {
         this.semester = semester;
     }
 
+    @Column(name = "type", nullable = false)
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    @Column(name = "preRequisites", nullable = false)
+    public String getPreRequisites() { return preRequisites; }
+    public void setPreRequisites(String preRequisites) { this.preRequisites = preRequisites; }
+
+    @Column(name = "partOfDay", nullable = false)
+    public String getPartOfDay() { return partOfDay; }
+    public void setPartOfDay(String partOfDay) { this.partOfDay = partOfDay; }
+
+    @Column(name = "time", nullable = false)
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Column(name = "day", nullable = false)
+    public String getDay() { return day; }
+    public void setDay(String day) { this.day = day; }
+
+    @Column(name = "faculty", nullable = false)
+    public String getFaculty() { return faculty; }
+    public void setFaculty(String faculty) { this.faculty = faculty; }
+
+
     @Override
     public String toString() {
-        return "Employee [id=" + id + ", courseName=" + courseName + ", time=" + time + ", semester=" + semester
-       + "]";
+        return "Courses{" +
+                "id=" + id +
+                ", courseNumber='" + courseNumber + '\'' +
+                ", courseName='" + courseName + '\'' +
+                ", semester='" + semester + '\'' +
+                ", type='" + type + '\'' +
+                ", preRequisites='" + preRequisites + '\'' +
+                ", partOfDay='" + partOfDay + '\'' +
+                ", time='" + time + '\'' +
+                ", day='" + day + '\'' +
+                ", faculty='" + faculty + '\'' +
+                '}';
     }
- 
 }
